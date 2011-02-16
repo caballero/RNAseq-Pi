b0c13698d2f5be85eee1714c66e6929c8b3817f3
Juan Caballero <jcaballero@systemsbiology.org>
2011-02-16 15:28:38 -0800

    remove duplicate SAM parser -> getUnmapSam.pl/parseSAM.pl

--- bin/getUnmapSam.pl
+++ bin/getUnmapSam.pl
@@ -1,35 +1,37 @@
-#!/usr/bin/perl -w
-use strict;
+#!/usr/bin/perl
 
 =head1 NAME
 
-getUnampSam.pl 
+parseSAM.pl
 
 =head1 DESCRIPTION
 
-Read a SAM file, get the unmapped reads, write a Fastq/Fasta file with them.
+Read a SAM file input and separate mapped and unmapped reads (mapped reads are
+reported in SAM format, unmapped reads can be reported as Fasta/Fastq).
 
 =head1 USAGE
 
-perl getUnmapSam.pl < SAM > FASTQ
+perl parseSAM.pl [OPTIONS] < SAM > OUTPUT
 
 OPTIONS:
-   Parameter       Description                  Values     Default
-   -i --input      Read sequences from here     File*      STDIN
-   -o --output     Write sequences here         File       STDOUT
-   -m --mapped     Write mapped reads here      FILE       
-   -f --format     Output format (Fastq/Fasta)  fq/fa      fq
-   -g --group      Write sequences in batches   int        100000
-   -v --verbose    Verbose mode
-   -h --help       Print help
+   Parameter        Description               Values          Default
+   -i --input       Input SAM file*           File name       STDIN
+   -o --output      Output file               File name       STDOUT
+   -f --format      Output format             fa/fq           fq
+   -m --mapped      Write mapped reads here   File name
+   -q --qual        Use this quality score    QS              B
+   -v --verbose     Verbose mode
+   -h --help        Print this screen
    
-   *File can be compressed (gzip/bzip2) but requires option -i
+   * File can be compressed (gzip/bzip2) but requires option -i
    
 =head1 EXAMPLES
 
-perl getUnmapSam.pl < SAM > FASTQ
+perl parseSAM.pl < align.sam > unmapped_reads.fq
 
-perl getUnmapSam.pl -f fa -i SAM -o FASTA
+perl parseSAM.pl -i align.sam.gz -f fa -m mapped.sam -o unmapped_reads.fa
+
+samtools view align.bam | perl parseSAM.pl > unmmaped_reads.fq
 
 =head1 AUTHOR
 
@@ -58,42 +60,38 @@ along with code.  If not, see <http://www.gnu.org/licenses/>.
 =cut
 
 use strict;
+use warnings;
 use Getopt::Long;
 use Pod::Usage;
 
 # Default parameters
-my $help     = undef;         # print help
-my $verbose  = undef;         # verbose mode
-my $format   = 'fq';          # sequence format
-my $input    = undef;         # input SAM file
-my $output   = undef;         # output file
-my $batch    = 100000;        # size of the batch
-my $mapped   = undef;         # mapped file output
+my $help    = undef;    # print help
+my $verbose = undef;    # verbose mode
+my $format  = 'fq';     # sequence format
+my $input   = undef;    # input file
+my $output  = undef;    # output file
+my $mapped  = undef;    # report mapped reads here
+my $dqual   = 'B';      # default quality score
 
 # Main variables
-my $id       = undef;         # Sequence ID
-my $bit      = undef;         # SAM bit flag
-my $seq      = undef;         # Sequence
-my $qual     = undef;         # Fastq qualities
-my %seqs     = ();            # Hash to keep sequences in memory
-$seqs{'num'} = 0;             # Sequences counter
-my $nbatch   = 0;             # Counter for batches
-my @arr      = ();            # array for data
+my $num_mapped   = 0;
+my $num_unmapped = 0;
+my $total        = 0;
 
 # Calling options
 GetOptions(
-    'h|help'           => \$help,
-    'v|verbose'        => \$verbose,
-    'i|input:s'        => \$input,
-    'o|output:s'       => \$output,
-    'f|format:s'       => \$format,
-    'b|batch:i'        => \$batch,
-    'm|mapped:s'       => \$mapped
+    'h|help'      => \$help,
+    'v|verbose'   => \$verbose,
+    'i|input:s'   => \$input,
+    'f|format:s'  => \$format,
+    'o|output:s'  => \$output,
+    'm|mapped:s'  => \$mapped,
+    'q|qual:s'    => \$dqual
 ) or pod2usage(-verbose => 2);
-    
+
 pod2usage(-verbose => 2) if (defined $help);
 
-# opening files (if required)
+# Opening files (if required)
 if (defined $input) {
     $input = "gunzip  -c $input | " if ($input =~ m/gz$/);
     $input = "bunzip2 -c $input | " if ($input =~ m/bz2$/);
@@ -103,57 +101,65 @@ if (defined $output) {
     open STDOUT, ">$output" or die "cannot write file $output\n";
 }
 if (defined $mapped) {
-    open MAP, ">$mapped" or die "cannot write file $mapped\n";
+    open MAPPED, ">$mapped" or die "cannot write file $mapped\n";
 }
 
+# Reading SAM file
 while (<>) {
-    next if (m/^\@/); # skip SAM headers
-    @arr  = split (/\t/, $_);
-    $id   = $arr[0];
-    next if (defined $seqs{'used'}{$id}); # No duplicates
-    $bit  = $arr[1];
-    $seq  = $arr[8];
-    $qual = $arr[9];
-    # Only unmapped reads (flag = 4)
-    unless ($bit == 4) {
-        print MAP $_ if (defined $mapped);
-        $seqs{'map'}++;
-        $seqs{'used'}{$id} = 1;
-        next;
-    }
-    $seqs{'num'}++;
-    $seqs{'used'}{$id} = 1;
-    
-    if ($format eq 'fq') {
-        $seqs{'seqs'} .= "\@$id\n$seq\n+\n$qual\n";
-    }
-    elsif ($format eq 'fa') {
-        $seqs{'seqs'} .= ">$id\n$seq\n";
+    # catch SAM header
+    if (/^\@/) {
+        print MAPPED $_ if (defined $mapped);
     }
     else {
-        die "Sorry, I don't recognize this format: $format\n";
+        my @line = split (/\t/, $_);
+        my $flag = $line[1];
+        
+        if ($flag % 4 == 0) { # unmapped read
+            my $id   = $line[0];
+            my $seq  = $line[9];
+            my $qual = $line[10];
+            printSeq($id, $seq, $qual);
+            $num_unmapped++;
+        }
+        else { # mapped read
+            print MAPPED $_ if (defined $mapped);
+            $num_mapped++;
+        }
     }
-    
-    flushSeqs() if ($seqs{'num'} >= $batch);
 }
-flushSeqs();
 
-# Print report in verbose mode 
-warn "found $nbatch unmapped sequences\n" if (defined $verbose);
-warn "found $seqs{'map'} mapped sequences\n" if (defined $verbose);
+if (defined $verbose) {
+    my $total     = $num_mapped + $num_unmapped;
+    my $per_map   = sprintf( "%.2f", 100 * $num_mapped   / $total);
+    my $per_unmap = sprintf( "%.2f", 100 * $num_unmapped / $total);
+    warn <<__INFO__    
+# Total sequences    = $total
+# Mapped sequences   = $num_mapped ($per_map\%)
+# Unmapped sequences = $num_unmapped ($per_unmap\%)
+__INFO__
+;
+}
 
 ###################################
 ####   S U B R O U T I N E S   ####
 ###################################
 
-# flushSeqs -> print sequences
-# Call: flushSeqs()
-# Return: nothing
-sub flushSeqs  {
-    $nbatch += $seqs{'num'};
-    warn "processed $nbatch seqs\n" if (defined $verbose);
-    print $seqs{'seqs'} if (defined $seqs{'seqs'});
-    $seqs{'num'}  = 0;
-    $seqs{'seqs'} = '';
-}
+# printSeq -> print a Fasta/Fastq sequence
+# call: printSeq( $seq_id, $seq_nuc, $seq_qual )
+# return: nothing
 
+sub printSeq {
+    my ($i, $s, $q) = @_;
+    if ($format eq 'fa') {
+        print ">$i\n$s\n";
+    }
+    elsif ($format eq 'fq') {
+        if ($q eq '*') {
+            $q = $dqual x length $s;
+        }
+        print "\@$i\n$s\n+\n$q\n";
+    }
+    else {
+        die "format is not recognized: $format\n";
+    }
+}
