a786fa35a402d1263258aacde377f9950f01078e
Juan Caballero <jcaballero@systemsbiology.org>
2011-02-23 13:37:12 -0800

    Paired-End SAM parser added -> getUnmapSam-PE.pl

--- bin/getUnmapSam-PE.pl
+++ bin/getUnmapSam-PE.pl
@@ -2,12 +2,14 @@
 
 =head1 NAME
 
-parseSAM.pl
+getUnmapSam-PE.pl
 
 =head1 DESCRIPTION
 
 Read a SAM file input and separate mapped and unmapped reads (mapped reads are
 reported in SAM format, unmapped reads can be reported as Fasta/Fastq).
+
+Special version for paired-end sequences.
 
 =head1 USAGE
 
@@ -16,22 +18,25 @@ perl parseSAM.pl [OPTIONS] < SAM > OUTPUT
 OPTIONS:
    Parameter        Description               Values          Default
    -i --input       Input SAM file*           File name       STDIN
-   -o --output      Output file               File name       STDOUT
+   -p --prefix      Output file prefix**      File name       
+   -1 --out1        Output file pair 1        File name
+   -2 --out2        Output file pair 2        File name
    -f --format      Output format             fa/fq           fq
    -m --mapped      Write mapped reads here   File name
    -q --qual        Use this quality score    QS              B
    -v --verbose     Verbose mode
    -h --help        Print this screen
    
-   * File can be compressed (gzip/bzip2) but requires option -i
+   *  File can be compressed (gzip/bzip2) but requires option -i
+   ** Prefix value will be used to create 2 output files: PRE-1.fq, PRE-2.fq
    
 =head1 EXAMPLES
 
-perl parseSAM.pl < align.sam > unmapped_reads.fq
+perl parseSAM.pl -p unmap_pair < align.sam
 
-perl parseSAM.pl -i align.sam.gz -f fa -m mapped.sam -o unmapped_reads.fa
+perl parseSAM.pl -i align.sam.gz -f fa -m mapped.sam -1 unmap_1.fa -2 unmap_2.fa
 
-samtools view align.bam | perl parseSAM.pl > unmapped_reads.fq
+samtools view align.bam | perl parseSAM.pl -p unmapped_reads
 
 =head1 AUTHOR
 
@@ -69,14 +74,16 @@ my $help    = undef;    # print help
 my $verbose = undef;    # verbose mode
 my $format  = 'fq';     # sequence format
 my $input   = undef;    # input file
-my $output  = undef;    # output file
+my $prefix  = undef;    # prefix name for outputs
+my $out1    = undef;    # output 1
+my $out2    = undef;    # output 2
 my $mapped  = undef;    # report mapped reads here
 my $dqual   = 'B';      # default quality score
 
 # Main variables
 my $num_mapped   = 0;
-my $num_unmapped = 0;
-my $total        = 0;
+my $num_unmap1   = 0;
+my $num_unmap2   = 0;
 
 # Calling options
 GetOptions(
@@ -84,12 +91,15 @@ GetOptions(
     'v|verbose'   => \$verbose,
     'i|input:s'   => \$input,
     'f|format:s'  => \$format,
-    'o|output:s'  => \$output,
+    'p|prefix:s'  => \$prefix,
+    '1|out1:s'    => \$out1,
+    '2|out2:s'    => \$out2,
     'm|mapped:s'  => \$mapped,
     'q|qual:s'    => \$dqual
 ) or pod2usage(-verbose => 2);
 
 pod2usage(-verbose => 2) if (defined $help);
+pod2usage(-verbose => 2) unless (defined $prefix or (defined $out1 and defined $out2));
 
 # Opening files (if required)
 if (defined $input) {
@@ -97,12 +107,17 @@ if (defined $input) {
     $input = "bunzip2 -c $input | " if ($input =~ m/bz2$/);
     open STDIN, "$input" or die "cannot read file $input\n";
 }
-if (defined $output) {
-    open STDOUT, ">$output" or die "cannot write file $output\n";
-}
 if (defined $mapped) {
     open MAPPED, ">$mapped" or die "cannot write file $mapped\n";
 }
+
+if (defined $prefix) {
+    $out1 = "$prefix-1.$format";
+    $out2 = "$prefix-2.$format";
+}
+open O1, ">$out1" or die "cannot write file $out1\n";
+open O2, ">$out2" or die "cannot write file $out2\n";
+
 
 # Reading SAM file
 while (<>) {
@@ -113,13 +128,19 @@ while (<>) {
     else {
         my @line = split (/\t/, $_);
         my $flag = $line[1];
+        my $id   = $line[0];
+        my $seq  = $line[9];
+        my $qual = $line[10];
         
-        if ($flag == 4) { # unmapped read
-            my $id   = $line[0];
-            my $seq  = $line[9];
-            my $qual = $line[10];
-            printSeq($id, $seq, $qual);
-            $num_unmapped++;
+        if ($flag == 77) { # unmapped read 1
+            my $fseq = formatSeq($id, $seq, $qual);
+            print O1 $r;
+            $num_unmap1++;
+        }
+        elsif ($flag == 141) { # unmapped read 2
+            my $fseq = formatSeq($id, $seq, $qual);
+            print O2 $r;
+            $num_unmap2++;
         }
         else { # mapped read
             print MAPPED $_ if (defined $mapped);
@@ -129,13 +150,10 @@ while (<>) {
 }
 
 if (defined $verbose) {
-    my $total     = $num_mapped + $num_unmapped;
-    my $per_map   = sprintf( "%.2f", 100 * $num_mapped   / $total);
-    my $per_unmap = sprintf( "%.2f", 100 * $num_unmapped / $total);
     warn <<__INFO__    
-# Total sequences    = $total
-# Mapped sequences   = $num_mapped ($per_map\%)
-# Unmapped sequences = $num_unmapped ($per_unmap\%)
+# Mapped sequences = $num_mapped
+# Unmapped seqs #1 = $num_unmap1
+# Unmapped seqs #2 = $num_unmap1
 __INFO__
 ;
 }
@@ -148,18 +166,20 @@ __INFO__
 # call: printSeq( $seq_id, $seq_nuc, $seq_qual )
 # return: nothing
 
-sub printSeq {
+sub formatSeq {
     my ($i, $s, $q) = @_;
+    my $r = undef;
     if ($format eq 'fa') {
-        print ">$i\n$s\n";
+        $r = ">$i\n$s\n";
     }
     elsif ($format eq 'fq') {
         if ($q eq '*') {
             $q = $dqual x length $s;
         }
-        print "\@$i\n$s\n+\n$q\n";
+        $r = "\@$i\n$s\n+\n$q\n";
     }
     else {
         die "format is not recognized: $format\n";
     }
+    return $r;
 }
