48b4516e293fa4f34a66930e08d615f849b74c5b
Juan Caballero <jcaballero@systemsbiology.org>
2011-06-20 16:53:29 -0700

    verbose info

--- bin/cov2block.pl
+++ bin/cov2block.pl
@@ -109,9 +109,9 @@ if (defined $output) {
     warn "Writing output in:  $output\n" if (defined $verbose);
     open STDOUT, ">$output" or die "cannot open $output\n";
 }
-warn "Window size         = $window\n" if (defined $verbose);
-warn "Step size           = $step\n" if (defined $verbose);
-warn "Minimal coverage    = $mincov\n" if (defined $verbose);
+warn "Window size       = $window\n" if (defined $verbose);
+warn "Step size         = $step\n" if (defined $verbose);
+warn "Minimal coverage  = $mincov\n" if (defined $verbose);
 
 while (<>) {
     chomp;
