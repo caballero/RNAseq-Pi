21596f1e3e941e815415a874b83473967e7eb7b5
Juan Caballero <jcaballero@systemsbiology.org>
2011-06-20 16:43:52 -0700

    print SD too

--- bin/cov2block.pl
+++ bin/cov2block.pl
@@ -169,7 +169,8 @@ sub newBlock {
 sub printBlock {
     if($block{'num'} >= $step) {
         $block{'mean'} = sprintf("%.2f", mean(@{ $block{'cov'} }));
-        print join ("\t", $block{'seq'}, $block{'ini'}, $block{'end'}, $block{'mean'});
+        $block{'sd'}   =   sd($block{'mean'}, @{ $block{'cov'} });
+        print join ("\t", $block{'seq'}, $block{'ini'}, $block{'end'}, $block{'mean'}, $block{'sd'});
         print "\n";
     }
 }
